234fce46ba262b784f4c94a4e51782175b185d12
Abel Nieva Saavedra <abel.nieva.saavedra@gmail.com>
2019-10-01 10:00:23 +0200

    Implementará el servicio config para obtener las variables de entorno
    Se implementa el servicio config que permite obtener las variables de entorno
    desde un fichero .env.
    
    Tarea: SHA-7

---
+++ src/config/config.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+
+import * as dotenv from 'dotenv';
+import * as Joi from '@hapi/joi';
+import * as fs from 'fs';
+
+export interface EnvConfig {
+  [key: string]: string;
+}
+
+@Injectable()
+export class ConfigService {
+  private readonly envConfig: EnvConfig;
+
+  constructor(filePath: string) {
+    const config = dotenv.parse(fs.readFileSync(filePath));
+    this.envConfig = this.validateInput(config);
+  }
+
+  get(key: string): string {
+    return this.envConfig[key];
+  }
+
+  get isApiAuthEnabled(): boolean {
+    return Boolean(this.envConfig.API_AUTH_ENABLED);
+  }
+
+  /**
+   * Ensures all needed variables are set, and returns the validated JavaScript
+   * object including the applied default values.
+   */
+  private validateInput(envConfig: EnvConfig): EnvConfig {
+    const envVarsSchema: Joi.ObjectSchema = Joi.object({
+      NODE_ENV: Joi.string()
+        .valid('development', 'production', 'test', 'provision')
+        .default('development'),
+      PORT: Joi.number().default(3000),
+      API_AUTH_ENABLED: Joi.boolean().required(),
+      DATABASE_USER: Joi.string().required(),
+      DATABASE_PASSWORD: Joi.string().required(),
+    });
+    const { error, value: validatedEnvConfig } = envVarsSchema.validate(
+      envConfig,
+    );
+
+    if (error) {
+      throw new Error(`Config validation error: ${error.message}`);
+    }
+
+    return validatedEnvConfig;
+  }
+}
